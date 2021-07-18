1468d05a906d43dd77660bd7a432d8b40ca18626
GordienkoEE <gordienko.e.evg@sberbank.ru>
2021-07-18 10:35:54 +0300

    add first client

--- src/main/java/ru/learning/httpasyncclient/HttpAsyncClientApplication.java
+++ src/main/java/ru/learning/httpasyncclient/HttpAsyncClientApplication.java
@@ -1,13 +1,33 @@
 package ru.learning.httpasyncclient;
 
+import org.asynchttpclient.*;
+import org.asynchttpclient.util.HttpConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class HttpAsyncClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HttpAsyncClientApplication.class, args);
+		DefaultAsyncHttpClientConfig.Builder clientBuilder = Dsl.config()
+				.setConnectTimeout(500);
+		AsyncHttpClient client = Dsl.asyncHttpClient(clientBuilder);
+		Request request = new RequestBuilder(HttpConstants.Methods.GET)
+				.setUrl("http://www.baeldung.com")
+				.build();
+		ListenableFuture<Response> listenableFuture = client
+				.executeRequest(request);
+		listenableFuture.addListener(() -> {
+			try {
+				Response response = listenableFuture.get();
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+		}, Executors.newCachedThreadPool());
 	}
 
 }
