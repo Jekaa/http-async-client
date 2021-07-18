4ae763b6f48ebfbac9a0632e0820a934838d10b6
GordienkoEE <gordienko.e.evg@sberbank.ru>
2021-07-18 10:26:49 +0300

    init commit

---
+++ src/main/java/ru/learning/httpasyncclient/HttpAsyncClientApplication.java
@@ -0,0 +1,13 @@
+package ru.learning.httpasyncclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpAsyncClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpAsyncClientApplication.class, args);
+	}
+
+}
